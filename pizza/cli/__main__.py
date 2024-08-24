235ff42fce5e836c6d23917db2d230935412fb7b
iiPython <ben@iipython.dev>
2024-08-24 13:24:52 -0500

    finish stuff up

--- pizza/cli/__main__.py
+++ pizza/cli/__main__.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2024 iiPython
 
 # Modules
-# import subprocess
+import subprocess
 from pathlib import Path
 
 import click
@@ -12,6 +12,7 @@ from mutagen import MutagenError  # type: ignore
 from mutagen.flac import FLAC
 
 from .cache import cache
+from .. import __version__
 
 # Initialization
 session, lrclib, base_url = Session(), LRCLib(), "https://pizza.iipython.dev"
@@ -34,116 +35,30 @@ def pizza() -> None:
     Code available at https://github.com/iiPythonx/pizza."""
     return
 
-# Commands
-# @pizza.command(help = "Perform a metadata search")
-# @click.argument("path")
-# @click.option("--bpm", is_flag = True, show_default = True, default = False, help = "Include song BPM in metadata")
-# @click.option("--lyrics", is_flag = True, show_default = True, default = False, help = "Include lyrics from LRCLIB")
-# @click.option("--force", is_flag = True, show_default = True, default = False, help = "Force write metadata")
-# def update(path: str, bpm: bool, lyrics: bool, force: bool) -> None:
-#     full_path = Path(path)
-#     if not full_path.is_dir():
-#         return click.secho("✗ Specified path does not exist.", fg = "red")
+@pizza.group("cache")
+def _cache() -> None:
+    """Handle changing the Pizza cache from the CLI."""
+    return
 
-#     for file in full_path.rglob("*"):
-#         if not (file.is_file() and file.suffix == ".flac"):
-#             continue
+@_cache.command(help = "Clear all items in the cache.")
+def clear() -> None:
+    cache.cache = {}
+    return click.secho("✓ Cache erased.", fg = "green")
 
-#         # Load data into mutagen
-#         try:
-#             metadata = FLAC(file)
-#             if "PIZZA" in metadata and not force:
-#                 continue
+@_cache.command(help = "Dumps all items in the cache to stdout.")
+def dump() -> None:
+    return print(cache.cache)
 
-#             # Calculate artist
-#             artist = metadata.get("ALBUMARTIST", metadata.get("ARTIST"))
-#             if artist is None:
-#                 click.secho(f"⚠ Skipping '{file}' due to missing ARTIST tag.", fg = "yellow")
-#                 continue
+@pizza.command(help = "Check the current pizza version.")
+def version() -> None:
+    return click.secho(f"Pizza v{__version__} by iiPython", fg = "blue")
 
-#             album = metadata.get("ALBUM")
-#             if album is None:
-#                 click.secho(f"⚠ Skipping '{file}' due to missing ALBUM tag.", fg = "yellow")
-#                 continue
-
-#             artist, album = artist[0], album[0]
-
-#             # Let's go boys!
-#             response = cache.find_response(artist, album)
-#             if response is None:
-#                 response = session.post(f"{base_url}/api/find", params = {"artist": artist, "album": album}).json()
-#                 cache.set_response(artist, album, response)
-
-#             if response["code"] != 200:
-#                 click.secho(f"⚠ Request failed for '{file}'.", fg = "yellow")
-#                 continue
-
-#             response = response["data"]
-#             if response is None:
-#                 click.secho(f"⚠ No match found for '{file}'.", fg = "yellow")
-#                 continue
-
-#             # Check what we have to match with
-#             title, track = metadata.get("TITLE"), metadata.get("TRACK")
-#             title, track = title[0] if title is not None else None, track[0] if track is not None else None
-#             if not (title or track):
-#                 click.secho(f"⚠ No data to match with for '{file}'.", fg = "yellow")
-#                 continue
-
-#             match = list(filter(lambda x: x["title"] == title or str(x["position"]) == track, response["tracks"]))
-#             if not match:
-#                 click.secho(f"⚠ No match found for '{file}'.", fg = "yellow")
-#                 continue
-
-#             match = match[0]
-#             metadata.clear()
-
-#             # Metadata assignments
-#             if response["date"] is not None:
-#                 metadata["YEAR"] = response["date"].split("-")[0]
-#                 metadata["DATE"] = response["date"]
-
-#             metadata["DISC"]        = str(match["disc"])
-#             metadata["ALBUM"]       = response["album"]
-#             metadata["TRACK"]       = str(match["position"])
-#             metadata["TITLE"]       = match["title"]
-#             metadata["ARTIST"]      = match["artist"]
-#             metadata["ALBUMARTIST"] = response["artist"]
-#             metadata["MUSICBRAINZ_ALBUMID"] = response["ids"]["album"]
-#             metadata["MUSICBRAINZ_ALBUMARTISTID"] = response["ids"]["artist"]
-
-#             # Fetch lyrics
-#             if lyrics is True:
-#                 result = lrclib.get(match["title"], response["artist"][0], response["album"], metadata.info.length)
-#                 if result is not None:
-#                     final_lyrics = result.get("syncedLyrics", result.get("plainLyrics")) or result.get("plainLyrics")
-#                     if final_lyrics is not None:
-#                         metadata["LYRICS"] = final_lyrics
-
-#             # Calculate BPM
-#             if bpm is True:
-#                 ffmpeg = subprocess.Popen(
-#                     ["ffmpeg", "-vn", "-i", file, "-ar", "44100", "-ac", "1", "-f", "f32le", "pipe:1"],
-#                     stdout = subprocess.PIPE,
-#                     stderr = subprocess.DEVNULL
-#                 )
-#                 result = subprocess.check_output(["bpm"], stdin = ffmpeg.stdout)
-#                 ffmpeg.wait()
-#                 metadata["BPM"] = str(round(float(result.removesuffix(b"\n"))))
-
-#             # File writing! How fun!
-#             metadata["PIZZA"] = "managed"
-#             metadata.save()
-#             click.secho(f"✓ Assigned metadata to '{file}'.", fg = "green")
-
-#         except MutagenError:
-#             click.secho(f"⚠ Failed loading file '{file}'.", fg = "yellow")
-#             continue
-
-@pizza.command()
+@pizza.command(help = "Perform a metadata search.")
 @click.argument("path")
+@click.option("--bpm", is_flag = True, show_default = True, default = False, help = "Include song BPM in metadata")
+@click.option("--lyrics", is_flag = True, show_default = True, default = False, help = "Include lyrics from LRCLIB")
 @click.option("--force", is_flag = True, show_default = True, default = False, help = "Force write metadata")
-def create_hash(path: str, force: bool) -> None:
+def update(path: str, bpm: bool, lyrics: bool, force: bool) -> None:
     full_path = Path(path)
     if not full_path.is_dir():
         return click.secho("✗ Specified path does not exist.", fg = "red")
@@ -184,8 +99,8 @@ def create_hash(path: str, force: bool) -> None:
                 existing_album = import_list[-1]
 
             existing_album["tracks"].append((
-                metadata.get("TITLE", [None])[0],
-                metadata.get("TRACK", [None])[0],
+                metadata.get("TITLE", [None])[0],  # type: ignore | idk why pylance hates this but ohk
+                metadata.get("TRACK", [None])[0],  # type: ignore | idk why pylance hates this but ohk
                 file
             ))
 
@@ -231,9 +146,10 @@ def create_hash(path: str, force: bool) -> None:
                 click.secho(f"  > No match found for '{file.name}'.", fg = "yellow")
                 continue
 
-            # Save metadata!!!11
             metadata = FLAC(file)
             metadata.clear()
+
+            # Begin writing metadata
             for field, function in field_index.items():
                 metadata[field] = function(response, match)
 
@@ -241,5 +157,26 @@ def create_hash(path: str, force: bool) -> None:
                 metadata["YEAR"] = response["date"].split("-")[0]
                 metadata["DATE"] = response["date"]
 
+            # Fetch lyrics
+            if lyrics is True:
+                result = lrclib.get(match["title"], response["artist"][0], response["album"], metadata.info.length)
+                if result is not None:
+                    final_lyrics = result.get("syncedLyrics", result.get("plainLyrics")) or result.get("plainLyrics")
+                    if final_lyrics is not None:
+                        metadata["LYRICS"] = final_lyrics
+
+            # Calculate BPM
+            if bpm is True:
+                ffmpeg = subprocess.Popen(
+                    ["ffmpeg", "-vn", "-i", file, "-ar", "44100", "-ac", "1", "-f", "f32le", "pipe:1"],
+                    stdout = subprocess.PIPE,
+                    stderr = subprocess.DEVNULL
+                )
+                result = subprocess.check_output(["bpm"], stdin = ffmpeg.stdout)
+                ffmpeg.wait()
+                metadata["BPM"] = str(round(float(result.removesuffix(b"\n"))))
+
+            # File writing
+            metadata["PIZZA"] = "managed"
             metadata.save()
             click.secho(f"  > Updated metadata for '{file.name}'.", fg = "green")
